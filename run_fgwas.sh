5a187afe424f639bee7c781fd62fa2fc5300c0c9
WSL - Roberto Olvera-Hernandez <rolvhdez@ccg.unam.mx>
2025-06-16 11:06:27 -0600

    [changed] corrected syntax errors; the script needs to create the 'sumstats' directory first

--- run_fgwas.sh
+++ run_fgwas.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-$pheno_id=$1
-$estimator=$2
+mkdir -p "${out_dir%/}/sumstats/"
+pheno_id=$1
+estimator=$2
 
 # cycle for every 3 autosomes in decreasing order
 i=22 # last autosome
+
+# Define out_dir if not already set
+out_dir=${out_dir:-.}
+
 while [[ i -gt 0 ]]; do
-	j = $(( i-2 ))
+	j=$(( i-2 ))
 	if [ $j -lt 1 ]; then 
 		# last chromosome is run separately
 		./fgwas/"$estimator".sh $pheno_id $i
