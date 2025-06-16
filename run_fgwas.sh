442b378a7a816061b9dbb7de8d1da7e04c9286d0
WSL - Roberto Olvera-Hernandez <rolvhdez@ccg.unam.mx>
2025-06-16 10:42:36 -0600

    [added] script to run a given FGWAS script automatically, and in parallel

---
+++ run_fgwas.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+$pheno_id=$1
+$estimator=$2
+
+# cycle for every 3 autosomes in decreasing order
+i=22 # last autosome
+while [[ i -gt 0 ]]; do
+	j = $(( i-2 ))
+	if [ $j -lt 1 ]; then 
+		# last chromosome is run separately
+		./fgwas/"$estimator".sh $pheno_id $i
+		exit 0
+	else
+		# run the FGWAS in parallel
+		parallel \
+			--joblog "${out_dir%/}/sumstats/"$estimator"_joblog_$j-$i.log" \
+			-j 3 ./fgwas/"$estimator".sh {1} {2} ::: $pheno_id ::: $(seq $i -1 $j)
+		i=$((j-1))
+	fi
+done
