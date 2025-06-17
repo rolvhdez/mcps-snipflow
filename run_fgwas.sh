c09fcab02fdd5fbfd5dd5d5bfdd149deb802cb13
WSL - Roberto Olvera-Hernandez <rolvhdez@ccg.unam.mx>
2025-06-16 22:42:05 -0600

    [changed] this one worked

--- run_fgwas.sh
+++ run_fgwas.sh
@@ -4,42 +4,35 @@ mkdir -p "${out_dir%/}/sumstats/"
 pheno_id=$1
 estimator=$2
 
-# Cycle for every 3 autosomes in decreasing order
-chr_i=22
+# Start and end chromosomes
+chr_i=7
 min_chr=1
-while [[ chr_i -ge $min_chr ]]; do
-	# Read the number of variants for a range of chromosomes
-	chr_j=$((chr_i-2))
-	n_vars=0
-	for x in $(seq $chr_i -1 $chr_j); do
-		vars=$(awk -v chr="$x" '$1 == chr' "${bed}.bim" | wc -l)
-		n_vars=$((n_vars+vars))
-	done
 
-	# Optimized for 64 cores / 256GB RAM
-	if [[ n_vars -le 35000 ]]; then
-		# Small chromosomes (14-22)
-		cpu=8
-		threads=2
+# Computational power
+cpu=8
+threads=2
+
+# Cycle for each autosome in decreasing order
+while [[ chr_i -ge $min_chr ]]; do
+
+	if [[ chr_i -ge 8 ]]; then
+		# every 3 autosomes
+		chr_j=$((chr_i-2))
 		batch_size=100000
-	elif [[ n_vars -le 60000 ]]; then
-		# Medium chromosomes (10-13)
-		cpu=4
-		threads=2
+		n_jobs=3
+	else
+		# every 2 autosomes
+		chr_j=$((chr_i-1))
 		batch_size=50000
-	else 
-		# Large chromosomes (1-7)
-		cpu=6
-		threads=1
-		batch_size=12500
+		n_jobs=2
 	fi
 
 	# Execute the FGWAS script
 	if [[ $chr_j -ge $min_chr ]]; then
 		#echo "${chr_i} ${chr_j} ${n_vars} ${cpu} ${threads} ${batch_size}"
 		parallel \
-			--joblog "${out_dir%/}/sumstats/"$estimator"_joblog_$j-$i.log" \
-			-j 3 ./fgwas/"$estimator".sh {1} {2} {3} {4} {5} \
+			--joblog "${out_dir%/}/sumstats/"$estimator"_joblog_$chr_j-$chr_i.log" \
+			-j $n_jobs ./fgwas/"$estimator".sh {1} {2} {3} {4} {5} \
 			::: $pheno_id \
 			::: $(seq $chr_i -1 $chr_j) \
 			::: $cpu \
