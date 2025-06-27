c64a05dd9d03f5aeb09c37954a8f6f65fa18164a
Roberto Olvera Hernandez <rolvhdez@ccg.unam.mx>
2025-06-27 17:45:27 -0600

    [new] sequencially run for a list of phenotype ids

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
-nextflow run main.nf -resume \
-    -params-file "./params.json"
+
+input_json="./base_params.json"
+tmp_json="/tmp/params.json"
+phenotype_ids=()
+out_dir_base="/path/to/output/directory"
+
+for phen_id in "${phenotype_ids[@]}"; do
+    # Create the output directory for the current phenotype
+    phen_dir="${out_dir_base}/phenotype-${phen_id}"
+    mkdir -p "${phen_dir}"
+
+    # Create a temporary JSON file with the updated phenotype index
+    jq --arg phen_id "$phen_id" '.phenoIndex += ($phen_id | tonumber)' "$input_json" > "$tmp_json"
+    jq --arg out_dir "${phen_dir}" '.outDir = $out_dir' "$tmp_json" > "$tmp_json.tmp" && mv "$tmp_json.tmp" "$tmp_json"
+
+    # Write the used params file
+    cat "$tmp_json" > "${phen_dir}/params.json"
+
+    # Run the Nextflow pipeline with the updated parameters
+    nextflow run main.nf -resume \
+        -params-file "$tmp_json" \
+        -with-report "${phen_dir}/report.html" \
+        -with-timeline "${phen_dir}/timeline.html" \
+        -with-trace "${phen_dir}/trace.txt"
+done
